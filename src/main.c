31fa5d94f870f8a4b3ea37c04d8282b81035c1d5
miguelpiedade2007 <miguel,piedade2007@gmail.com>
2026-04-23 13:35:41 -0300

    Adição da função do cálculo de média no código

--- src/main.c
+++ src/main.c
@@ -1,14 +1,37 @@
 #include<stdio.h>
 
+//Função para a opção 1: cálculo de média
+void calculo_media() {
+    float nota1, nota2, media;
+    printf("--- Calcular media ---\n");
+    printf("Digite a nota 1: ");
+    scanf("%f", &nota1);
+    printf("Digite a nota 2: ");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2)/2; 
+    printf("A sua media e: %.2f\n", media);
+
+}
+
+
 
 int main() {
    
+
+
+
 
 
 //menu da interface no CMD
 
 
 int opcao;
+
+
+
+
+
 
 
 printf("---MENU da equipe ARM---\n");
