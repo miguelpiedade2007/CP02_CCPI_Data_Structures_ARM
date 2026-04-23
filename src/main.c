ca599d8b19bdc10626d0b9c6d3b93a8125a773e5
miguelpiedade2007 <miguel,piedade2007@gmail.com>
2026-04-23 13:20:49 -0300

    adição do menu interativo no terminal

--- src/main.c
+++ src/main.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
 
-int main() {
-    
 
-    
+int main() {
+   
+
+
+//menu da interface no CMD
+
+
+int opcao;
+
+
+printf("---MENU da equipe ARM---\n");
+printf("1-Calcular media\n");
+printf("2-Verificar situacao\n");
+printf("3-Calcular derivada\n");
+printf("4-Encerrar programa\n");
+printf("Escolha uma das opcoes: ");
+scanf("%d", &opcao);
+
+
+
+
+
+
+
+
+
+
+   
     return 0;
 }
+
