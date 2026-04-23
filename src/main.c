c87e2c6866daec2dfb1f24c361f9dffcd1f32dc5
Zanizada <rafaelgizzi@gmail.com>
2026-04-23 14:10:49 -0300

    Criação da função "menu()" para otimizar o código e atualização do .gitignore para ignorar arquivos da IDE VsCode.

--- src/main.c
+++ src/main.c
@@ -14,44 +14,20 @@ void calculo_media() {
 
 }
 
-
+void menu() {
+    printf("---MENU da equipe ARM---\n");
+    printf("1-Calcular media\n");
+    printf("2-Verificar situacao\n");
+    printf("3-Calcular derivada\n");
+    printf("4-Encerrar programa\n");
+}
 
 int main() {
-   
-
-
-
-
-
 //menu da interface no CMD
-
-
 int opcao;
-
-
-
-
-
-
-
-printf("---MENU da equipe ARM---\n");
-printf("1-Calcular media\n");
-printf("2-Verificar situacao\n");
-printf("3-Calcular derivada\n");
-printf("4-Encerrar programa\n");
+menu();
 printf("Escolha uma das opcoes: ");
 scanf("%d", &opcao);
-
-
-
-
-
-
-
-
-
-
-   
     return 0;
 }
 
